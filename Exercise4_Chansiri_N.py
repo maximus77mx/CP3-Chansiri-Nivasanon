50b9fc43ca014275cdec3cdc1cef3500229f53a4
maximus77mx <76804616+maximus77mx@users.noreply.github.com>
2021-01-02 17:28:16 +0700

    Anwser

---
+++ Exercise4_Chansiri_N.py
@@ -0,0 +1,10 @@
+fe = 90.1
+gb = 45.5
+ics = 76.8
+cp = 77.77
+
+print("--- Your Score ---")
+print("Foundation English :" ,fe)
+print("General Business :" , gb)
+print("Introduction to Computer Systems :" , ics)
+print("Computer Programming ;" , cp)
